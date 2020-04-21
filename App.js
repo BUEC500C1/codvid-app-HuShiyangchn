d097b238b3ffc43ff11590abc28a8bb107b4fbf1
HuShiyangchn <55211567+HuShiyangchn@users.noreply.github.com>
2020-04-20 23:46:06 -0400

    apiupdate

--- App.js
+++ App.js
@@ -2,6 +2,31 @@ import React from 'react';
 import { StyleSheet, Text, View , TextInput} from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
 
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        var status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
+    };
+    xhr.send();
+};
+
+getJSON('https://api.covid19api.com/world/total',
+    function(err, data) {
+        if (err !== null) {
+            alert('Something went wrong: ' + err);
+        } else {
+            alert('Your query count: ' + data.query.count);
+        }
+    });
+
+
 export default class App extends React.Component {
     onRegionChange(region) {
         this.setState({ region });
